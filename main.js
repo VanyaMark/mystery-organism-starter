15c4c74c9f84d689ccaad9c5d2b8155c775ba05a
VanyaMark <markova.vanya@gmail.com>
2024-01-15 20:17:42 +0100

    pAequorFactory factory function done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,10 +13,14 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Factory Function that returns an object that contains the properties specimenNum and dna that correspond to the parameters provided
+pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum: specimenNum,
+    dna: dna
+  }
+}
 
 
-
-
-
-
-
+console.log(pAequorFactory(1, mockUpStrand()))
+console.log(mockUpStrand())
